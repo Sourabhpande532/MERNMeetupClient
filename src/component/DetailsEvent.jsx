002809e7346c6ffc6f70b57c6eab1d44ecd24960
sourabhpande532 <sourabhpande43@gmail.com>
2025-10-13 20:10:33 +0530

    Feat: build details page and get id params

---
+++ src/component/DetailsEvent.jsx
@@ -0,0 +1,11 @@
+import { Header } from "./Header"
+
+export const DetailsEvent = ()=>{
+    return(
+        <div className="container">
+         <Header/>
+         <hr/>
+         <section>Details of page</section>
+        </div>
+    )
+}
