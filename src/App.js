5e9358783dc047f96f081549509c6c2c2e75f598
sourabhpande532 <sourabhpande43@gmail.com>
2025-10-14 13:35:59 +0530

    Feat: process to build search

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
 import { Header } from './component/Header';
 import { MeetupEvents } from './component/MeetupEvents';
+import { useState } from 'react';
+
 function App() {
+  // Shared states for both component
+  const [search, setSearch] = useState( "Tech Conference" );
+  const [type, setType] = useState( "Both" )
   return (
     <div className="container bg-color">
-     <Header/>
-     <hr/>
-     <MeetupEvents/>
+      {/* Pass props to both component */ }
+      <Header search={ search } setSearch={ setSearch } />
+      <hr />
+      <MeetupEvents search={ search } type={ type } setType={ setType } />
     </div>
   );
 }
--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,19 +1,12 @@
 import { NavLink } from "react-router-dom"
-import useFetch from "../useFetch"
-import { useState } from "react"
 
-export const Header = () => {
-    const [search,setSearch] = useState('Tech Conference');
-    console.log(search);
-    const { data, loading } = useFetch( `http://localhost:4000/events/title/${search}` )
-    console.log(data);
-
+export const Header = ( { search, setSearch } ) => {
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
             <div className="container-fluid">
                 <NavLink className="navbar-brand" to="/">Meetup</NavLink>
             </div>
-            <form className="d-flex" role="search">
+            <form className="d-flex" role="search" onSubmit={ ( e ) => e.preventDefault() }>
                 <input className="form-control me-2" type="search" placeholder="Search by title and tag" aria-label="Search"
                     onChange={ ( e ) => setSearch( e.target.value ) }
                 />
